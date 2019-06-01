7092dcb4faf1f377ce888e5f5d969751fd562d83
santiago <santiagolampropulos@gmail.com>
2019-05-31 23:47:19 -0300

    arreglo algo para ver travis

--- test/SaludadorTest.java
+++ test/SaludadorTest.java
@@ -3,5 +3,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SaludadorTest {
+    @Test
+    void saludadorSaludaAUnaPersona(){
+        Saludador saludador = new Saludador();
+        String saludo = saludador.saludar();
+
+        assertEquals(saludo, "hola");
+    }
 
 }
