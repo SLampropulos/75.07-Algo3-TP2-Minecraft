a66bb74cbe4a389cee0def6c9f9d7291c5d8f411
Matias <mhuenul@fi.uba.ar>
2019-06-01 20:28:10 -0300

    Hice un par de tests. Luego agrego los que faltan

---
+++ test/MaderaTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class MaderaTest {
+
+    @Test
+    public void maderaEsGolpeadaPorPicoYSeLanzaExcepcion() {
+        Madera madera = new Madera();
+        float durabilidad = madera.getDurabilidad();
+        /* Acá iría la clase Pico y debería funcionar igual. Como no está implementada todavía
+        * la dejo así. */
+        Herramienta pico = new Herramienta(200, 10);
+
+        assertThrows(HerramientaInvalidaException.class, ()-> {madera.desgastarCon(pico);});
+    }
+
+    @Test
+    public void maderaEsGolpeadaPorHachaMaderaYSeDesgasta() {
+        Madera madera = new Madera();
+        float durabilidad = madera.getDurabilidad();
+        HachaMadera hacha = new HachaMadera();
+
+        madera.desgastarCon(hacha);
+
+        assertEquals(durabilidad - 2, madera.getDurabilidad());
+    }
+
+    @Test
+    public void maderaEsGolpeadaPorHachaPiedraYSeDesgasta() {
+        Madera madera = new Madera();
+        float durabilidad = madera.getDurabilidad();
+        HachaPiedra hacha = new HachaPiedra();
+
+        madera.desgastarCon(hacha);
+
+        assertEquals(durabilidad - 5, madera.getDurabilidad());
+    }
+}
