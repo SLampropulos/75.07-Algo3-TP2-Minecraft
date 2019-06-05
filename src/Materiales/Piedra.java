e0f0fe3143098efed71d47cf152c60c266dec743
Distance71 <48816876+Distance71@users.noreply.github.com>
2019-06-05 04:53:20 -0300

    Update Piedra.java

--- src/Materiales/Piedra.java
+++ src/Materiales/Piedra.java
@@ -4,23 +4,24 @@ import Herramientas.PicoMadera;
 import Herramientas.PicoMetal;
 import Herramientas.PicoPiedra;
 
+static final int DURABILIDAD_PIEDRA 30
 
 public class Piedra extends Material {
 
     public Piedra() {
-        this.durabilidad = 30;
+        this.durabilidad = DURABILIDAD_PIEDRA;
     }
 
     public void desgastarCon(PicoMadera pico) {
-        this.desgastar(pico.getDesgaste());
+        this.desgastar(pico.obtenerDesgaste());
     }
 
     public void desgastarCon(PicoPiedra pico) {
-        this.desgastar(pico.getDesgaste());
+        this.desgastar(pico.obtenerDesgaste());
     }
 
     public void desgastarCon(PicoMetal pico) {
-        this.desgastar(pico.getDesgaste());
+        this.desgastar(pico.obtenerDesgaste());
     }
 
 }
