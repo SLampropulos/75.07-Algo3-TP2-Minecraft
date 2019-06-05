4ad42afead02c63b7aed37676b7e650c61eb5e0b
Distance71 <48816876+Distance71@users.noreply.github.com>
2019-06-05 04:38:20 -0300

    Update PicoMetal.java

--- src/Herramientas/PicoMetal.java
+++ src/Herramientas/PicoMetal.java
@@ -1,11 +1,15 @@
 package Herramientas;
 
-import Herramientas.Desgastador.DesgatadorPorUsos;
+import Herramientas.Desgastador.DesgastadorPorUsos;
+
+//Definición de constantes
+static final int DURABILIDAD_PICO_METAL 400;
+static final int FUERZA_PICO_METAL 12;
 
 public class PicoMetal extends Herramienta{
 
     public PicoMetal() {
-        super(400, 12);
-        desgastador = new DesgatadorPorUsos(10,400);
+        super(DURABILIDAD_PICO_METAL, FUERZA_PICO_METAL);
+        desgastador = new DesgastadorPorUsos(10, 400);
     }
 }
