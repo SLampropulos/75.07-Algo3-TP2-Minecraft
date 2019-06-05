88dea6e06de63620981c07fc5f979d7fb8a64259
Distance71 <48816876+Distance71@users.noreply.github.com>
2019-06-05 04:51:08 -0300

    Update Madera.java

--- src/Materiales/Madera.java
+++ src/Materiales/Madera.java
@@ -4,22 +4,24 @@ import Herramientas.HachaMadera;
 import Herramientas.HachaMetal;
 import Herramientas.HachaPiedra;
 
+static final int DURABILIDAD_MADERA 10;
+
 public class Madera extends Material {
 
     public Madera() {
-        this.durabilidad = 10;
+        this.durabilidad = DURABILIDAD_MADERA;
     }
 
     public void desgastarCon(HachaMadera hacha) {
-        this.desgastar(hacha.getDesgaste());
+        this.desgastar(hacha.obtenerDesgaste());
     }
 
     public void desgastarCon(HachaPiedra hacha) {
-        this.desgastar(hacha.getDesgaste());
+        this.desgastar(hacha.obtenerDesgaste());
     }
 
     public void desgastarCon(HachaMetal hacha) {
-        this.desgastar(hacha.getDesgaste());
+        this.desgastar(hacha.obtenerDesgaste());
     }
 
 }
