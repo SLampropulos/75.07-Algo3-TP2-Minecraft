3c8af8c04b1c74c44c71e7a335a8694b56f11f01
Hugo Huzan <hhuzan@gmail.com>
2019-06-08 07:17:07 -0300

    agrego .bat para ejecutar juego

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -1,8 +1,9 @@
-package vista;
+
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import vista.ContenedorPrincipal;
 
 
 public class Aplicacion extends Application  {
