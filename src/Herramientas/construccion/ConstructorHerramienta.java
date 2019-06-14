f7a7defe6be4f5a00e88d1b5eade86f7f304ee5c
hhuzan <hhuzan@DESKTOP-QM5K8PV>
2019-06-13 21:39:05 -0300

    Subo comentado ConstructorHerramienta

---
+++ src/Herramientas/construccion/ConstructorHerramienta.java
@@ -0,0 +1,21 @@
+package Herramientas.construccion;
+
+import Herramientas.construccion.patrones.PatronConstruccion;
+
+public class ConstructorHerramienta {
+//	private List<PatronConstruccion> patronesDisponibles= new ArrayList<PatronConstruccion>();
+//	
+//	public ConstructorHerramienta() {
+//		patronesDisponibles.add(new PatronPicoFino());
+//		...
+//	}
+//	
+//	public Herramienta construir(PatronConstruccion patronDibujado) {
+//		for(PatronConstruccion patronDisponible : patronesDisponibles){
+//			if (patronDisponible.equals(patronDibujado){
+//				return patronDisponible.crearHerramienta();
+//			}
+//		}
+//		
+//	}
+}
---
+++ src/Herramientas/construccion/patrones/PatronPicoFino.java
@@ -0,0 +1,14 @@
+package Herramientas.construccion.patrones;
+
+public class PatronPicoFino {
+//
+//	public PatronPicoFino{
+//		//patron=sfdsdgsdfsdfdf
+//		
+//	}
+//	
+//	@override
+//	public Herramienta crearHerramienta() {
+//		return new PicoFino();
+//	}
+}
