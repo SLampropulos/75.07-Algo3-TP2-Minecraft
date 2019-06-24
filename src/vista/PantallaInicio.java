03b4d9c8c8f704e2186da7f4b728e1c4b0df4902
Distance71 <48816876+Distance71@users.noreply.github.com>
2019-06-24 00:55:03 -0300

    Add files via upload

--- src/vista/PantallaInicio.java
+++ src/vista/PantallaInicio.java
@@ -1,19 +1,27 @@
 package vista;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+import javafx.scene.media.*;
+import java.io.File;
+
 import eventos.InicioDelJuegoHandler;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import juego.MineCraft;
+
 
 public class PantallaInicio extends StackPane {
     
-	//Definición de constantes
+	//Definicion de constantes
 	static final String BOTON_INICIO = "INICIAR JUEGO";
 	static final String DIRECCION_FONDO_MENU_INICIAL = "file:src/vista/images/inicio.jpg";
-	static final int TAMAÑO_HORIZONTAL_PANTALLA_INICIAL = 1160;
-	static final int TAMAÑO_VERTICAL_PANTALLA_INICIAL = 640;
+	static final int TAMANIO_HORIZONTAL_PANTALLA_INICIAL = 1160;
+	static final int TAMANIO_VERTICAL_PANTALLA_INICIAL = 640;
 	static final String DIRECCION_AUDIO_INICIO = "sounds/INTRO.mp3";
 	
 	//Constructor
@@ -24,7 +32,7 @@ public class PantallaInicio extends StackPane {
         botonIniciar.setOnAction(new InicioDelJuegoHandler(juego, stage));
         
     	this.getChildren().add(botonIniciar);
-        BackgroundImage backgroundImage = new BackgroundImage(new Image(DIRECCION_FONDO_MENU_INICIAL, TAMAÑO_HORIZONTAL_PANTALLA_INICIAL, TAMAÑO_VERTICAL_PANTALLA_INICIAL, false, true),
+        BackgroundImage backgroundImage = new BackgroundImage(new Image(DIRECCION_FONDO_MENU_INICIAL, TAMANIO_HORIZONTAL_PANTALLA_INICIAL, TAMANIO_VERTICAL_PANTALLA_INICIAL, false, true),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
         this.setBackground(new Background(backgroundImage));
