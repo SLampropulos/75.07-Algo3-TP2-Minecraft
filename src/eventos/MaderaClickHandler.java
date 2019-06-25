fed1020e68c9733a11a82ed287f15267abf6f0da
hhuzan <hhuzan@DESKTOP-QM5K8PV>
2019-06-25 13:49:25 -0300

    update MaderaClickHandler

--- src/eventos/MaderaClickHandler.java
+++ src/eventos/MaderaClickHandler.java
@@ -5,20 +5,28 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.StageStyle;
+import juego.MineCraft;
+import vista.ContenedorPrincipal;
 
 public class MaderaClickHandler implements EventHandler<MouseEvent> {
+	private MineCraft mineCraft;
+	private ContenedorPrincipal contenedorPrincipal;
 
-	public MaderaClickHandler() {
-		
+	public MaderaClickHandler(MineCraft mineCraft, ContenedorPrincipal contenedorPrincipal) {
+		this.mineCraft = mineCraft;
+		this.contenedorPrincipal = contenedorPrincipal;
 	}
-	
-    @Override
-    public void handle(MouseEvent event) {
+
+	@Override
+	public void handle(MouseEvent event) {
+		mineCraft.getJugador();
+		contenedorPrincipal.refrescar();
+		
 		Alert dialogoAlerta = new Alert(AlertType.INFORMATION);
 		dialogoAlerta.setTitle("");
 		dialogoAlerta.setHeaderText(null);
 		dialogoAlerta.setContentText("Click Madera");
 		dialogoAlerta.initStyle(StageStyle.UTILITY);
 		dialogoAlerta.showAndWait();
-    }
+	}
 }
