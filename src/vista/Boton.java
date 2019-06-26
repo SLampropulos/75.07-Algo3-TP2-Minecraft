0d4d9592117b1e3c788c77895ca7e750d7606895
Distance71 <48816876+Distance71@users.noreply.github.com>
2019-06-26 00:43:03 -0300

    Elimino "strings magicos"

--- src/vista/Boton.java
+++ src/vista/Boton.java
@@ -5,33 +5,40 @@ import javafx.scene.control.Button;
 
 public class Boton extends Button {
 
+    static final String TEXTO_BOTON_ARRIBA = "Arriba";
+    static final String TEXTO_BOTON_ABAJO = "Abajo";
+    static final String TEXTO_BOTON_IZQUIERDA = "Izquierda";
+    static final String TEXTO_BOTON_DERECHA = "Derecha";
+    static final String TEXTO_BOTON_CONSTRUIR = "Construir";
+    static final String TEXTO_BOTON_CANCELAR = "Cancelar";
+    
     public Boton(BotonArribaHandler handler) {
-        setText("Arriba");
+        setText(TEXTO_BOTON_ARRIBA);
         setOnAction(handler);
     }
 
     public Boton(BotonAbajoHandler handler) {
-        setText("Abajo");
+        setText(TEXTO_BOTON_ABAJO);
         setOnAction(handler);
     }
 
     public Boton(BotonIzquierdaHandler handler) {
-        setText("Izquierda");
+        setText(TEXTO_BOTON_IZQUIERDA);
         setOnAction(handler);
     }
 
     public Boton(BotonDerechaHandler handler) {
-        setText("Derecha");
+        setText(TEXTO_BOTON_DERECHA);
         setOnAction(handler);
     }
 
     public Boton(BotonConstruirHandler handler) {
-        setText("Construir");
+        setText(TEXTO_BOTON_CONSTRUIR);
         setOnAction(handler);
     }
 
     public Boton(BotonCancelarHandler handler) {
-        setText("Cancelar");
+        setText(TEXTO_BOTON_CANCELAR);
         setOnAction(handler);
     }
 
